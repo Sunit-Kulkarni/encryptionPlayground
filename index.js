77ce00d4490f0e851c91c9a07e91a549d54ef307
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-14 01:02:17 -0400

    got encryption to work but debugging decryption

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
-
+const secrets = require('./config/keys');
 openpgp.initWorker({ path: 'openpgp.worker.js' }); // set the relative web worker path
 
 // put keys in backtick (``) to avoid errors caused by spaces or tabs
-const pubkey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
-...
------END PGP PUBLIC KEY BLOCK-----`;
-const privkey = `-----BEGIN PGP PRIVATE KEY BLOCK-----
-...
------END PGP PRIVATE KEY BLOCK-----`; //encrypted private key
-const passphrase = `yourPassphrase`; //what the privKey is encrypted with
+const pubkey = secrets.testPubKey;
+const privkey = secrets.privkey; //encrypted private key
+//const passphrase = `yourPassphrase`; //what the privKey is encrypted with
 
 const encryptDecryptFunction = async () => {
-  const privKeyObj = openpgp.key.readArmored(privkey).keys[0];
-  await privKeyObj.decrypt(passphrase);
+  //const privKeyObj = openpgp.key.readArmored(privkey).keys[0];
+  //await privKeyObj.decrypt(passphrase);
 
   const options = {
     data: 'Hello, World!', // input as String (or Uint8Array)
-    publicKeys: openpgp.key.readArmored(pubkey).keys, // for encryption
-    privateKeys: [privKeyObj] // for signing (optional)
+    publicKeys: openpgp.key.readArmored(pubkey).keys // for encryption
   };
 
   openpgp
     .encrypt(options)
     .then(ciphertext => {
       encrypted = ciphertext.data; // '-----BEGIN PGP MESSAGE ... END PGP MESSAGE-----'
-      return encrypted;
+      //return encrypted;
+      console.log(ciphertext.data);
     })
     .then(encrypted => {
       const options = {
         message: openpgp.message.readArmored(encrypted), // parse armored message
-        publicKeys: openpgp.key.readArmored(pubkey).keys, // for verification (optional)
-        privateKeys: [privKeyObj] // for decryption
+        privateKeys: privkey // for decryption
       };
 
       openpgp.decrypt(options).then(plaintext => {
